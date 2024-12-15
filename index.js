aacdd88e64619fa7ecfdef8ae43d5b6a1ec5205c
z4y4nP <zayan.funtime@gmail.com>
2024-12-15 18:38:22 +0000

    undefined;)

---
+++ index.js
@@ -0,0 +1,8 @@
+function multiply(x,y){
+    if(y === undefined){
+        alert ("y is undefined value");
+        y == 7;
+    }
+    var answer = x * y;
+document.getElementById("answer").innerHTML = answer;
+}
